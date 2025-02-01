a71b5e4dd4c423e938e2520990748fa26c354596
undertry <zzzizy@proton.me>
2025-02-01 12:58:41 -0300

    Areglo Modulo Cava Polybar + 2 Monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,21 +6,20 @@ killall polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar logo -c $(dirname $0)/config.ini &
-polybar workspaces -c $(dirname $0)/config.ini &
-polybar window -c $(dirname $0)/config.ini &
-polybar target -c $(dirname $0)/config.ini &
-polybar music -c $(dirname $0)/config.ini &
-polybar wlan -c $(dirname $0)/config.ini &
-polybar ip -c $(dirname $0)/config.ini &
-polybar cpu -c $(dirname $0)/config.ini &
-polybar memory -c $(dirname $0)/config.ini &
-polybar volume -c $(dirname $0)/config.ini &
-polybar date -c $(dirname $0)/config.ini &
-polybar systray -c $(dirname $0)/config.ini &
-polybar power -c $(dirname $0)/config.ini &
+# Launch polybar on the primary monitor (HDMI-3)
+MONITOR=HDMI-3 polybar logo -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar workspaces -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar target -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar music -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar wlan -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar ip -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar cpu -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar memory -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar volume -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar date -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar systray -c $(dirname $0)/config.ini &
+MONITOR=HDMI-3 polybar power -c $(dirname $0)/config.ini &
 
-if [[ $(xrandr -q | grep 'HDMI1 connected') ]]; then
-	polybar external -c $(dirname $0)/config.ini &
-fi
+# Launch polybar on the secondary (vertical) monitor (HDMI-1-2)
+MONITOR=HDMI-1-2 polybar v-workspaces -c $(dirname $0)/config.ini &
+MONITOR=HDMI-1-2 polybar v-window -c $(dirname $0)/config.ini &
