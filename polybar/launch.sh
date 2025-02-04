70fc5ade0c3d12ecac22f4592634284accf8103f
undertry <zzzizy@proton.me>
2025-02-04 10:15:10 -0300

    Modularización Polybar (Modules-Bars-Schemes)

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,19 +7,18 @@ killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar on the primary monitor (HDMI-3)
-MONITOR=HDMI-3 polybar logo -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar workspaces -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar target -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar music -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar wlan -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar ip -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar cpu -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar memory -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar volume -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar date -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar systray -c $(dirname $0)/config.ini &
-MONITOR=HDMI-3 polybar power -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar logo -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar workspaces -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar target -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar music -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar wlan -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar ip -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar cpu -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar memory -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar volume -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar date -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar systray -c $(dirname $0)/config.ini &
+MONITOR=HDMI-0 polybar power -c $(dirname $0)/config.ini &
 
 # Launch polybar on the secondary (vertical) monitor (HDMI-1-2)
-MONITOR=HDMI-1-2 polybar v-workspaces -c $(dirname $0)/config.ini &
-MONITOR=HDMI-1-2 polybar v-window -c $(dirname $0)/config.ini &
+# MONITOR=HDMI-1-2 polybar v-workspaces -c $(dirname $0)/config.ini &
