4197c031900bf6b9408832ac13af774f469c5960
undertry <zzzizy@proton.me>
2025-04-01 16:53:54 -0300

    Implementación starship, wofi y ajustes de hypr

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,7 @@
 if status is-interactive
-    # Limpia la pantalla al inicio
-    clear
+    starship init fish | source
     # Limpia mensaje de bienvenida
     set -U fish_greeting
-    # toilet -f mono9 -F gay "#!"
-    # cat ~/.config/fish/ascii/alert.txt | lolcat
     # alias
     alias ls="lsd"
     alias lsa="lsd -a"
@@ -18,12 +15,6 @@ if status is-interactive
     alias rebuild="sudo nixos-rebuild switch"
     alias nixos="sudo hx /etc/nixos/configuration.nix"
 
-    # # Mostrar un arte ASCII aleatorio al iniciar la terminal
-    # set ascii_files (ls ~/.config/fish/ascii/*.txt)
-    # set file_count (count $ascii_files)
-    # set random_index (math (random 1 $file_count)) # Generar índice aleatorio (1 a file_count)
-    # set random_ascii $ascii_files[$random_index] # Seleccionar archivo aleatorio
-    # cat $random_ascii
     set -Ux fish_user_paths $fish_user_paths ~/bin
 
 end
